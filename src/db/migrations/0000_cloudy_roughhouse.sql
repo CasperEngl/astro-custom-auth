a904aca25b29914accd4aed74ade173b47cb0150
Casper Engelmann <c.engelmann@custimy.io>
2024-08-24 19:45:57 +0200

    Remove initial migration files

--- src/db/migrations/0000_cloudy_roughhouse.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`email` text(50) NOT NULL,
-	`password` text(255) NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
