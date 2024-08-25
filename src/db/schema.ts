1b60467fe157939a21678de8a7988034faf4bae1
Casper Engelmann <c.engelmann@custimy.io>
2024-08-25 12:41:35 +0200

    Add generated full name field to Users table and add relations between Users and Sessions tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,18 @@
-import { sql } from "drizzle-orm";
+import type { SQL } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
+export const Users = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  fullName: text("full_name")
+    .notNull()
+    .generatedAlwaysAs(
+      (): SQL => sql`${Users.firstName} || ' ' || ${Users.lastName}`
+    ),
   createdAt: text("created_at")
     .default(sql`current_timestamp`)
     .notNull(),
@@ -13,9 +21,21 @@ export const users = sqliteTable("users", {
     .$onUpdate(() => sql`current_timestamp`),
 });
 
-export const sessions = sqliteTable("sessions", {
+export const Sessions = sqliteTable("sessions", {
   sessionId: text("session_id", { length: 64 }).primaryKey(),
-  userId: integer("user_id").notNull(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => Users.id),
   createdAt: text("created_at").default(sql`current_timestamp`),
-  expiresAt: text("expires_at").default(sql`current_timestamp`),
 });
+
+export const UsersRelations = relations(Users, ({ many }) => ({
+  sessions: many(Sessions),
+}));
+
+export const SessionsRelations = relations(Sessions, ({ one }) => ({
+  user: one(Users, {
+    fields: [Sessions.userId],
+    references: [Users.id],
+  }),
+}));
