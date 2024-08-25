00f669a7f27d7bcbfa2b5657542621b154081793
Casper Engelmann <c.engelmann@custimy.io>
2024-08-25 12:39:45 +0200

    Add type definitions for currentUser and flash message handling in environment configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare namespace App {
+  interface Locals {
+    currentUser?: {
+      id: number;
+      email: string;
+      firstName: string;
+      lastName: string;
+      fullName: string;
+    };
+    _flashMessages: Record<"success" | "error", Record<string, string[]>>;
+    flash: (type: "success" | "error", key: string, messages: string[]) => void;
+    getFlashes: (type: "success" | "error") => Record<string, string[]>;
+    getFlash: (type: "success" | "error", key: string) => string[] | null;
+    old: Record<string, FormDataEntryValue>;
+  }
+}
