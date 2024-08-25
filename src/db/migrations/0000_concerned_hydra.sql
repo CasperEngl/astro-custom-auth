35bbd2d15ff1523bc8ed564ac98c75af6801cc21
Casper Engelmann <c.engelmann@custimy.io>
2024-08-25 12:43:53 +0200

    Rename migration file and add new columns to the users table

--- src/db/migrations/0000_concerned_hydra.sql
+++ src/db/migrations/0000_concerned_hydra.sql
@@ -2,13 +2,16 @@ CREATE TABLE `sessions` (
 	`session_id` text(64) PRIMARY KEY NOT NULL,
 	`user_id` integer NOT NULL,
 	`created_at` text DEFAULT current_timestamp,
-	`expires_at` text DEFAULT current_timestamp
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`email` text NOT NULL,
 	`password` text NOT NULL,
+	`first_name` text NOT NULL,
+	`last_name` text NOT NULL,
+	`full_name` text NOT NULL GENERATED ALWAYS AS ("first_name" || ' ' || "last_name") VIRTUAL,
 	`created_at` text DEFAULT current_timestamp NOT NULL,
 	`updated_at` text DEFAULT current_timestamp
 );
