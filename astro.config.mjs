a27ce18cf4980ac423799ff0d08ebfee0643b64c
Casper Engelmann <c.engelmann@custimy.io>
2024-08-26 02:45:07 +0200

    Add node adapter and update integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -6,13 +7,16 @@ import simpleStackQuery from "simple-stack-query";
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: node({
+		mode: "standalone",
+	}),
 	output: "server",
 	integrations: [
+		simpleStackForm(),
+		simpleStackQuery(),
 		react(),
 		tailwind({
 			applyBaseStyles: false,
 		}),
-		simpleStackForm(),
-		simpleStackQuery(),
 	],
 });
