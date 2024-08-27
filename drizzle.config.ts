221f10580c3bd9687374033124bcb225d4916f3e
Casper Engelmann <c.engelmann@custimy.io>
2024-08-27 18:00:54 +0200

    Add DATABASE_URL validation to drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "drizzle-kit";
+import invariant from "invariant";
+
+invariant(process.env.DATABASE_URL, "DATABASE_URL is required");
 
 export default defineConfig({
 	dialect: "sqlite",
 	schema: "./src/db/schema.ts",
 	out: "./src/db/migrations",
 	dbCredentials: {
-		url: "./sqlite.db",
+		url: process.env.DATABASE_URL,
 	},
 });
