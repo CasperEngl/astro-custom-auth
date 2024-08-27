683ab37f06f13b06b1488d632254e07daf2ff653
Casper Engelmann <c.engelmann@custimy.io>
2024-08-27 20:27:05 +0200

    Add console.log to print process.env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 import invariant from "invariant";
 
+console.log(process.env);
+
 invariant(process.env.DATABASE_URL, "DATABASE_URL is required");
 
 export default defineConfig({
