d674a15ae1d35e3ab20b6dc3f8cce1287bae347c
Casper Engelmann <c.engelmann@custimy.io>
2024-08-27 18:00:47 +0200

    Add environment variable check for DATABASE_URL and create database if not set

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,13 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 import * as schema from "./schema";
+import invariant from "invariant";
 
-const sqlite = new Database("./sqlite.db");
+invariant(process.env.DATABASE_URL, "DATABASE_URL is not set");
+
+const sqlite = new Database(process.env.DATABASE_URL, {
+	create: true,
+});
 export const db = drizzle(sqlite, {
 	schema,
 });
