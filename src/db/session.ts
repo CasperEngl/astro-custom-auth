c2fbf7b4dbfcade1da81c72670ddaaf0661d7757
Casper Engelmann <c.engelmann@custimy.io>
2024-08-27 04:44:34 +0200

    Refactor session creation and destruction for better cookie handling

--- src/db/session.ts
+++ src/db/session.ts
@@ -1,16 +1,15 @@
-import cookie from "cookie";
+import Headers from "@mjackson/headers";
 import { eq } from "drizzle-orm";
 import ms from "ms";
 import { randomBytes } from "node:crypto";
 import { db } from ".";
 import { Sessions } from "./schema";
-import type { APIContext } from "astro";
 
 function createSessionId() {
 	return randomBytes(16).toString("hex");
 }
 
-export async function createSession(context: APIContext, userId: number) {
+export async function createSession(userId: number) {
 	const sessionId = createSessionId();
 
 	// Store the session in the database
@@ -21,25 +20,34 @@ export async function createSession(context: APIContext, userId: number) {
 		})
 		.run();
 
-	context.cookies.set("session", sessionId, {
-		httpOnly: true,
-		secure: import.meta.env.NODE_ENV === "production",
-		maxAge: ms("1 day") / 1000,
-		path: "/",
+	const headers = new Headers({
+		setCookie: [
+			{
+				name: "sessionId",
+				value: sessionId,
+				httpOnly: true,
+				secure: import.meta.env.NODE_ENV === "production" || undefined,
+				maxAge: ms("1 day") / 1000,
+				path: "/",
+			},
+		],
 	});
+
+	return headers.getSetCookie();
 }
 
 export async function getSession(request: Request) {
-	const parsedCookie = cookie.parse(request.headers.get("cookie") ?? "");
+	const headers = new Headers(request.headers);
+	const sessionId = headers.cookie.get("sessionId");
 
-	if (!parsedCookie.session) {
+	if (!sessionId) {
 		return null;
 	}
 
 	const [session] = await db
 		.select()
 		.from(Sessions)
-		.where(eq(Sessions.sessionId, parsedCookie.session));
+		.where(eq(Sessions.sessionId, sessionId));
 
 	return session;
 }
@@ -53,10 +61,17 @@ export async function destroySession(request: Request) {
 
 	await db.delete(Sessions).where(eq(Sessions.sessionId, session.sessionId));
 
-	return cookie.serialize("session", "", {
-		httpOnly: true,
-		secure: import.meta.env.NODE_ENV === "production",
-		maxAge: 0,
-		path: "/",
+	const headers = new Headers({
+		setCookie: [
+			{
+				name: "sessionId",
+				value: "deleted",
+				httpOnly: true,
+				secure: import.meta.env.NODE_ENV === "production" || undefined,
+				expires: new Date(0),
+				path: "/",
+			},
+		],
 	});
+	return headers.getSetCookie();
 }
