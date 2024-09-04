5d29a0b89a6a8d3fd8c148fa74728f4cfb1dd78f
Casper Engelmann <c.engelmann@custimy.io>
2024-09-04 16:59:42 +0200

    Add getInputErrors function to utils.ts file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
+import type { ErrorInferenceObject } from "astro/actions/runtime/utils.js";
+import { isInputError, type ActionError } from "astro:actions";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function getInputErrors<TInput extends ErrorInferenceObject>(
+	error?: ActionError<TInput> | null,
+) {
+	return error && isInputError(error) ? error.fields : null;
 }
