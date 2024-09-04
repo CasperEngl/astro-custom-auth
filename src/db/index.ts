97beab6cdd1965bed13e51675917c878fa88f97b
Casper Engelmann <c.engelmann@custimy.io>
2024-09-04 16:59:20 +0200

    Add dotenv configuration for environment variables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import Database from "better-sqlite3";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import invariant from "invariant";
